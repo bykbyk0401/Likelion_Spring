f09f6568a7ad3bf71f022f578cc64d4b70e58fa3
배영경 <baeyoungkyeong@baeyeong-gyeong-ui-MacBookPro.local>
2023-10-11 14:33:34 +0900

    [FEAT] add box/letter search API

--- src/main/java/com/likelion/springstudy/controller/HealthCheckController.java
+++ src/main/java/com/likelion/springstudy/controller/HealthCheckController.java
@@ -1,24 +1,16 @@
 package com.likelion.springstudy.controller;
 
+
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.HashMap;
-import java.util.Map;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api")//tomcat이 url mapping, endpoint 지정
+@RequestMapping("/api")
 public class HealthCheckController {
 
-    @GetMapping(value = "/health", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Map<String, String>> healthCheck() { //접근제어자 -> 제어자 순서로 , return type, 메소드 이름(파라미터), map은 dict와 유사
-        Map<String, String> map = new HashMap<>();
-        map.put("status", "OK");
-        map.put("message", "Health Check is OK");
-        return ResponseEntity.ok(map); //response entity 활용하자
+    @GetMapping(value = "/health", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.TEXT_PLAIN_VALUE)
+    public ResponseEntity<String> healthCheck() {
+        return ResponseEntity.ok("OK");
     }
 }
